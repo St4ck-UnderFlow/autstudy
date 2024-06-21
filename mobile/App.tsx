2a9b04ba21a4c588136ca7546df062cb39831e9b
arthurmousinho <arthursilvamo296@gmail.com>
2024-06-21 08:46:52 -0300

    feat: React native ui lib config

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "@/styles/global.css"
+
+import { StatusBar } from "expo-status-bar"
+import { ToastProvider } from "@/components/Toast"
+
+import { Profile } from "@/app/Profile"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <ToastProvider position="top">
+      <Profile />
+      <StatusBar style="light" />
+    </ToastProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/src/app/Profile.tsx
@@ -0,0 +1,25 @@
+import { Image, View, ScrollView } from "react-native"
+import { Input } from "@/components/Input"
+import { Button } from "@/components/Button"
+import { useToast } from "@/components/Toast"
+
+export function Profile() {
+  const { toast } = useToast()
+
+  return (
+    <View className="flex-1 bg-gray-700">
+      <ScrollView>
+        <Image
+          source={require("@/assets/banner.png")}
+          className="w-full h-52  -mb-16"
+        />
+        <View className="flex-1 px-4 pb-4">
+          <View className="w-full mt-6 flex-1">
+            <Input placeholder="Company" inputClasses="mb-6" label="Company" />
+            <Button label="Save" onPress={() => toast("Saved!")} />
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
